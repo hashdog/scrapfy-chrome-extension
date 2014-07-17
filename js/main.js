9ac2eca8ab772a88a7b85a35c6dd2b8fcb887772
Mario Colque <colquemario@gmail.com>
2014-07-17 10:58:51 -0300

    save the last lang selected

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 $(document).on('ready', function() {
+  var lastLang = localStorage.getItem('lastLang');
+
+  // Set the last lang selected
+  if (lastLang !== null) {
+    $('#lang-selector').val(lastLang);
+  }
+
   $('#create-btn').on('click', function(e) {
     e.preventDefault();
 
+    // Show loading spinner
     $('#loading-text').show();
     $('#create-text').hide();
 
+    // Get and save the selected lang
     var lang = $('#lang-selector').val();
-    $.post('http://api.scrapfy.io/scraps', {lang: lang}, function(data) {
-      chrome.tabs.create({url: data.url});
-    });
+    if (lang != lastLang) {
+      localStorage.setItem('lastLang', lang);
+    }
+
+    // Get the SCRAP data and open the new tab
+    $.post('http://api.scrapfy.io/scraps', {lang: lang})
+      .done(function(data) {
+        chrome.tabs.create({url: data.url});
+      });
   });
 });
